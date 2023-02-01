36a34a419e10269bc66be5b567f2e7c181b3a246
Mike Oram <mike.oram@mayden.co.uk>
2023-02-01 14:00:26 +0000

    extract hydrator from model file

--- API-Exercise/Models/ProductModel.cs
+++ API-Exercise/Models/ProductModel.cs
@@ -1,101 +1,60 @@
 ﻿using System;
 using API_Exercise.Services;
 
-namespace API_Exercise.Models
+namespace API_Exercise.Models;
+
+public interface IProduct
 {
+    public int id { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public double price { get; set; }
+    public float discountPercentage { get; set; }
+    public float rating { get; set; }
+    public int stock { get; set; }
+    public string brand { get; set; }
+    public string category { get; set; }
+    public string thumbnail { get; set; }
+    public List<string> images { get; set; }
+    public void changeCurrency(string currency);
+}
 
-    public interface IProductHydratorModel
+public class Product : IProduct
+{
+    public int id { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    private double _price;
+    public double price
     {
-        public List<IProduct>? getProducts();
-        public IProduct? getProductById(int id);
+        get { return _price * exchangeRates[currency]; }
+        set { _price = value; }
+    }
+    public float discountPercentage { get; set; }
+    public float rating { get; set; }
+    public int stock { get; set; }
+    public string brand { get; set; }
+    public string category { get; set; }
+    public string thumbnail { get; set; }
+    public List<string> images { get; set; }
+    private Dictionary<string, double> exchangeRates { get; }
+
+    private string currency = "GBP";
+    private readonly string[] validCurrencies = { "GBP", "EUR", "USD" };
+
+    public Product()
+    {
+        exchangeRates = new Dictionary<string, double>();
+        exchangeRates.Add("GBP", 1);
+        exchangeRates.Add("EUR", 1.14);
+        exchangeRates.Add("USD", 1.24);
     }
 
-    public class ProductHydratorModel : IProductHydratorModel
+    public void changeCurrency(string currency)
     {
-        public List<IProduct>? getProducts()
+        if (validCurrencies.Contains(currency))
         {
-            JsonFileReader reader = new JsonFileReader();
-            List<Product> products = reader.ReadAndParse<List<Product>>("products.json");
-            return products.Cast<IProduct>().ToList();
-        }
-
-        public IProduct? getProductById(int id)
-        {
-            List<IProduct> products = this.getProducts();
-            if (products == null)
-            {
-                return null;
-            }
-
-            IProduct? filteredProduct = null;
-
-            foreach (IProduct product in products)
-            {
-                if (product.id.Equals(id))
-                {
-                    filteredProduct = product;
-                    break;
-                }
-            }
-
-            return filteredProduct;
-        }
-
-    }
-
-    public interface IProduct
-    {
-        public int id { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-        public double price { get; set; }
-        public float discountPercentage { get; set; }
-        public float rating { get; set; }
-        public int stock { get; set; }
-        public string brand { get; set; }
-        public string category { get; set; }
-        public string thumbnail { get; set; }
-        public List<string> images { get; set; }
-        public void changeCurrency(string currency);
-    }
-
-    public class Product : IProduct
-    {
-        public int id { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-        private double _price;
-        public double price
-        {
-            get { return _price * exchangeRates[currency]; }
-            set { _price = value; }
-        }
-        public float discountPercentage { get; set; }
-        public float rating { get; set; }
-        public int stock { get; set; }
-        public string brand { get; set; }
-        public string category { get; set; }
-        public string thumbnail { get; set; }
-        public List<string> images { get; set; }
-        private Dictionary<string, double> exchangeRates { get; }
-
-        private string currency = "GBP";
-        private readonly string[] validCurrencies = { "GBP", "EUR", "USD" };
-
-        public Product()
-        {
-            exchangeRates = new Dictionary<string, double>();
-            exchangeRates.Add("GBP", 1);
-            exchangeRates.Add("EUR", 1.14);
-            exchangeRates.Add("USD", 1.24);
-        }
-
-        public void changeCurrency(string currency)
-        {
-            if (validCurrencies.Contains(currency))
-            {
-                this.currency = currency;
-            }
+            this.currency = currency;
         }
     }
 }
