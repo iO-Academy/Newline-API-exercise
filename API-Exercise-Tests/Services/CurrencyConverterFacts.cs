114c8f93d32ee9f93b381d7b9189ad55f01ea34d
Mike Oram <mike.oram@mayden.co.uk>
2023-02-07 09:02:37 +0000

    write unit tests for currency converter

---
+++ API-Exercise-Tests/Services/CurrencyConverterFacts.cs
@@ -0,0 +1,36 @@
+﻿using API_Exercise.Services;
+
+namespace API_Exercise_Tests;
+
+public class CurrencyConverterFacts
+{
+    [Fact]
+    public void CurrencyConverter_ReturnsTrueWithValidCurrency()
+    {
+        var result = CurrencyConverter.setCurrency("USD");
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CurrencyConverter_ReturnsFalseWithInvalidCurrency()
+    {
+        var result = CurrencyConverter.setCurrency("YEN");
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CurrencyConverter_ConvertsCurrencyCorrectly_USD()
+    {
+        CurrencyConverter.setCurrency("USD");
+        var result = CurrencyConverter.ConvertCurrency(1);
+        Assert.Equal(1.24, result);
+    }
+
+    [Fact]
+    public void CurrencyConverter_ConvertsCurrencyCorrectly_EUR()
+    {
+        CurrencyConverter.setCurrency("EUR");
+        var result = CurrencyConverter.ConvertCurrency(1);
+        Assert.Equal(1.14, result);
+    }
+}
